46c1dc21c437b2f4393ffb0f8829309a290e987c
IsabelaOtreira <81163788+IsabelaOtreira@users.noreply.github.com>
2021-07-31 05:01:02 -0300

    Exercício 2 consoles

---
+++ Consoles2/Corrida.java
@@ -0,0 +1,57 @@
+public class Corrida {
+	static int grilosf = 0;
+	public static int Pista = 0;
+	public static void Inicio(int QtGrilos, CtlThread[] thread, Grilo[] grilo) {
+        thread = new CtlThread[QtGrilos];
+
+        for(int i = 0; i < QtGrilos; i++) 
+        {
+            thread[i] = new CtlThread(grilo[i]);
+            thread[i].start();
+        }
+        for (int i = 0; i < thread.length; i++) 
+        {
+          try 
+          {
+            thread[i].join();
+          } 
+          catch (InterruptedException e ) 
+          {
+              e.printStackTrace();
+          }
+        }
+        while(grilosf < QtGrilos) {
+        Corre(QtGrilos, thread, grilo);
+        }
+    }
+
+    public static void Corre(int QtGrilos, CtlThread[] thread, Grilo[] grilo) {
+        for(int i = 0; i < QtGrilos; i++) 
+        {
+            if(grilo[i].acabou != true)
+            {
+                if(grilo[i].atposicao >= Pista) 
+                { 
+                grilosf ++;
+                System.out.println("O " + grilo[i].nome +" Chegou em " + grilo[i].pulinhos + " pulos") ;
+                grilo[i].acabou = true;
+                }
+                else 
+                {
+                    thread[i].run();
+                }
+                for (int i1 = 0; i1 < thread.length; i1++) 
+                {
+                  try 
+                  {
+                    thread[i1].join();
+                  } 
+                  catch (InterruptedException e ) 
+                  {
+                      e.printStackTrace();
+                  }
+                }
+        }
+        }
+    }
+}
---
+++ Consoles2/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+	static int QtGrilos = 0;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Scanner scanner = new Scanner(System.in);
+		  System.out.println("Quantos participantes a corrida vai ter? ");
+		  QtGrilos = scanner.nextInt();
+	    System.out.println("Grilos participantes " + QtGrilos);
+	    System.out.println("Quantos cms a corrida vai ter? ");
+	    Corrida.Pista = scanner.nextInt();
+			CtlThread threads[] = null;
+	      Grilo[] grilos = new Grilo[QtGrilos];
+
+	     for ( int i = 0; i < QtGrilos; i++) {
+	          grilos[i] = new Grilo("Grilo_" + (i+1));
+	      }
+	      Corrida.Inicio(QtGrilos, threads, grilos);
+
+	}
+
+}
